e39f7e4eb25d700e5206cace60b90a7e8f0bebcc
Ahmed bahgat <ahmdalbaz681@gmail.com>
2021-09-22 15:17:00 +0200

    Logo up

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -23,28 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: sizeFromWidth(context, 2),
-          height: sizeFromHeight(context, 2),
-          decoration: BoxDecoration(
-            color: clear_gray,
-            shape: BoxShape.circle,
-            border: Border.all(color: clear_gray,width: 1.5),
-            boxShadow: [BoxShadow(
-              color: clear_gray,
-              blurRadius: 3
-            )]
-          ),
-          child: CircleAvatar(
-              backgroundColor: white,
-              radius: 45,
-              child: Image(
-                  image: AssetImage(
-                'assets/Logo1.png',
-              ))),
-        ),
-      ),
+      body: Image(image:AssetImage("assets/logo2.png"))
     );
   }
 }
--- lib/widgets/logo.dart
+++ lib/widgets/logo.dart
@@ -3,17 +3,7 @@ import 'package:library_guide/constant/styles.dart';
 class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      backgroundColor: clear_gray,
-      radius: 47,
-      child: CircleAvatar(
-
-          backgroundColor: white,
-          radius: 45,
-          child:Image(image: AssetImage('assets/logo.png',))
-
-      ),
-    );
+    return     Image(image: AssetImage('assets/logo2.png',),height: 150,);
 
   }
 }
