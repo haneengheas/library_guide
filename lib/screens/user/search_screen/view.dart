b905dd80181b6a1ca88f405f8d5105ce43217347
haneengheas <haneengheas@gmail.com>
2021-09-23 16:00:34 +0200

    change color of bottom

--- lib/screens/user/search_screen/view.dart
+++ lib/screens/user/search_screen/view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:library_guide/screens/user/search_screen/input_search.dart';
+
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+   final TextEditingController searchController = TextEditingController();
 
   @override
   _SearchScreenState createState() => _SearchScreenState();
@@ -11,9 +13,8 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-
+        InputSearch(hint: 'الفئة التي تريد البحث عنها', controller: widget.searchController)
       ],
     );
   }
 }
-
