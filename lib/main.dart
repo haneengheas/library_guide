22e99640828e4555d842238155c8231109f3cd7b
haneengheas <haneengheas@gmail.com>
2021-10-09 20:38:00 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:library_guide/screens/splash_screen.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
     runApp(
     MyApp());}
 class MyApp extends StatelessWidget {
